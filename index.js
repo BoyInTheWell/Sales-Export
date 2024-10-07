1029cf5d5a0e645f545e3ce512ea4a37a5659bb1
Lucas <rockbarii2@gmail.com>
2024-10-07 13:49:37 -0300

    0.1

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+const REDIRECT_URI = 'https://boyinthewell.github.io/Sales-Export/';
+const CLIENT_ID = 'socLQDIifg0mqahAeMjDTcBo2xJITe3A';
+const STATE = '*Bpo2024*';
+const SCOPE = 'sales';
+const url = `https://api.contaazul.com/auth/authorize`
+
+
+async function fetchAsync (url) {
+    console.log("Starting up!");
+    
+    let response = await fetch(url + `?redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&scope=${SCOPE}&state=${STATE}`,
+       {method: "GET",
+        mode: "no-cors"
+       }
+    );
+    let data = response.json;
+    console.log(data);
+    
+    return data;
+  }
+
+fetchAsync (url)
+
+/* async function fetchAsync(url) {
+    console.log("Clicando");
+    
+    let response = await fetch(url + `?redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&scope=${SCOPE}&state=${STATE}`, 
+        {
+            method : "GET",
+            mode: 'no-cors'
+        }
+    );
+    let data = response;
+    console.log(data);
+};
+ 
+document.getElementById('send-call').addEventListener('click', fetchAsync(url));
+*/
