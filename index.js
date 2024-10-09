9bd10ed5671c8945f667c23e2a83ea5b3d963d32
Lucas <rockbarii2@gmail.com>
2024-10-09 20:09:59 -0300

    Version 2.0 ("A New Dawn")

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ async function create_Sales(endpoint, body) {
     alert("A sale was just created!")
 }
 
+function interpret_table( data ) {
+  
+    var table = data.split("\n");
+    
+    for (i in table) {
+        table[i] = table[i].split("\t");
+    }
+    
+    var keys = table.shift();
+    var objects = table.map(function(values) {
+    return keys.reduce(function(o, k, i) {
+        o[k] = values[i];
+        return o;
+    }, {});
+    });
+
+    return objects;
+}
+
 window.addEventListener('load', (e) => {
     const currentURLParams = new URL(window.location.href).searchParams
     if (currentURLParams.get("code") === null) {
@@ -79,4 +98,17 @@ window.addEventListener('load', (e) => {
         getToken(currentURLParams.get("code"))
         console.log(currentURLParams.get("code"));
     }
+})
+
+document.getElementById('send-table').addEventListener('click', (e) => {
+    const textarea_element = document.getElementById('table_paste').value;
+    const textarea_interpreted = interpret_table(textarea_element)
+
+
+    console.log(textarea_interpreted);
+    console.log(JSON.parse(textarea_interpreted[0]['Raw']));
+    
+    document.getElementById('main-container').append(`
+        Calm down folk
+        `)
 })
