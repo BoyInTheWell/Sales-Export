b9cb8e5dda9e9ea24abe07044637965931f4986e
Lucas <rockbarii2@gmail.com>
2024-10-09 17:23:28 -0300

    Version 1.2

--- index.js
+++ index.js
@@ -43,52 +43,35 @@ window.addEventListener('load', (e) => {
             console.log(response.json());
 
             ACESS_TOKEN = JSON.parse(response.json())['acess_token'];
+            REFRESH_TOKEN = JSON.parse(response.json())['refresh_token'];
             console.log(ACESS_TOKEN);
 
             alert("A sale is about to be created!");
             let corpo_regio = {
-                "number": 12,
-                "emission": "2024-10-09T19:46:10.096Z",
-                "status": "COMMITTED",
-                "customer_id": "62d05442-5e02-4fb3-978b-da7e58e1f770",
-                "seller_id": "62d05442-5e02-4fb3-978b-da7e58e1f771",
-                "products": [
-                  {
-                    "description": "Game Atari ET",
-                    "quantity": 2,
-                    "product_id": "f8ffb77a-3d52-42d7-9bec-ea38c0ef043d",
-                    "value": 50
-                  }
-                ],
-                "services": [
-                  {
-                    "description": "Fix car engine",
-                    "quantity": 1,
-                    "service_id": "e78c6d82-501a-4045-90c4-ae5b520f58dc",
-                    "value": 200
-                  }
-                ],
-                "discount": {
-                  "measure_unit": "VALUE",
-                  "rate": 5
-                },
-                "payment": {
-                  "type": "CASH",
-                  "method": "BANKING_BILLET",
-                  "installments": [
+                
+                    "emission": "2024-10-09T10:10:10.52Z",
+                    "status": "PENDING",
+                    "products":[
                     {
-                      "number": 1,
-                      "value": 305,
-                      "due_date": "2024-10-09T19:46:10.096Z",
-                      "status": "PENDING",
-                      "note": "NOTE",
-                      "hasBillet": true
+                    "description": "Atari Game Boy (Ultimate Deluxe Edition)",
+                    "quantity": 2,
+                    "value": 350
                     }
-                  ],
-                  "financial_account_id": "string"
-                },
-                "notes": "Sale made by noon",
-                "shipping_cost": 10
+                    ],
+                    "payment": {
+                    "type": "CASH",
+                    "method": "CASH",
+                    "installments": [
+                    {
+                    "number": 1,
+                    "value": 700,
+                    "due_date": "2024-10-11T10:10:12.12Z",
+                    "status": "PENDING"
+                    }
+                    ]
+                    
+                    }
+                    
               }
             create_Sales('/v1/sales', corpo_regio)
             
