e168e2c06165bccf50c550c04de37aac2824ce4a
Lucas <rockbarii2@gmail.com>
2024-10-10 09:02:10 -0300

    Version 2.04

--- index.js
+++ index.js
@@ -24,22 +24,24 @@ async function create_Sales(endpoint, body, token) {
         }
     )
     console.log(response.json());
+
+    return await response.json()
 }
 
-function interpret_table( data ) {
-  
+function interpret_table(data) {
+
     var table = data.split("\n");
-    
+
     for (i in table) {
         table[i] = table[i].split("\t");
     }
-    
+
     var keys = table.shift();
-    var objects = table.map(function(values) {
-    return keys.reduce(function(o, k, i) {
-        o[k] = values[i];
-        return o;
-    }, {});
+    var objects = table.map(function (values) {
+        return keys.reduce(function (o, k, i) {
+            o[k] = values[i];
+            return o;
+        }, {});
     });
 
     return objects;
@@ -52,18 +54,18 @@ window.addEventListener('load', (e) => {
             window.location.href = url + `auth/authorize?redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&scope=${SCOPE}&state=${STATE}`;
         });
     }
-    else{   
+    else {
         async function getToken(code) {
-        
-            let response = await fetch(`https://api.contaazul.com/oauth2/token?grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&code=${code}`, 
-            {
-                method : "POST",
-                headers: {
-                    'Authorization': `Basic ${clientIDandSecret}`,
-                },
-            }
-        );
-            
+
+            let response = await fetch(`https://api.contaazul.com/oauth2/token?grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&code=${code}`,
+                {
+                    method: "POST",
+                    headers: {
+                        'Authorization': `Basic ${clientIDandSecret}`,
+                    },
+                }
+            );
+
             json_response = await response.json()
             console.log(json_response);
             ACESS_TOKEN = json_response['access_token'];
@@ -80,7 +82,7 @@ window.addEventListener('load', (e) => {
 document.getElementById('send-token').addEventListener('click', (e) => {
     ACESS_TOKEN = document.getElementById('token_paste').value;
     console.log(ACESS_TOKEN);
-    
+
 })
 
 document.getElementById('send-table').addEventListener('click', (e) => {
@@ -90,28 +92,34 @@ document.getElementById('send-table').addEventListener('click', (e) => {
 
     console.log(textarea_interpreted);
     /* console.log(JSON.parse(textarea_interpreted[0]['Raw'])); */
-    
-    textarea_interpreted.forEach(e => {
+
+    textarea_interpreted.forEach((e, i) => {
         /* console.log(JSON.parse(e['Raw'])); */
-        JSON.parse(e['Raw']).forEach(i => {
-            const corpo_regio = {
-                "name": i['Name'],
-                "value": i['Unit_Value'],
-                "cost": 0,
-            }
-            create_Sales('v1/products', JSON.stringify(corpo_regio), ACESS_TOKEN)
-            console.log(corpo_regio);
-            
-        });
-        
-        const corpo_sanum ={
+
+        const corpo_sanum = {
             "name": e['Cliente'],
             "person_type": 'NATURAL',
             "document": e['Documento']
         }
-        create_Sales('v1/costumers', JSON.stringify(corpo_sanum), ACESS_TOKEN)
 
-    });
-    
- 
-})
+        create_Sales('v1/customers', JSON.stringify(corpo_sanum), ACESS_TOKEN).then(r => {
+
+            JSON.parse(i['Raw']).forEach(x => {
+                const corpo_regio = {
+                    "name": x['Name'],
+                    "value": x['Unit_Value'],
+                    "cost": 0,
+                }
+                create_Sales('v1/products', JSON.stringify(corpo_regio), ACESS_TOKEN).then(response => { console.log(response.json()) });
+
+            })
+
+            console.log(corpo_regio);
+
+        });
+
+    })
+
+});
+
+
